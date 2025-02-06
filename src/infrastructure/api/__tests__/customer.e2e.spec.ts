b2f030641959bd002deb224b6e4d4aca8011b3f0
viniciusidacruz <viniciusidacruz@hotmail.com>
2025-02-06 01:36:14 -0300

    feat: :sparkles: Finish all endpoints for module of customer
    Finish all endpoints for module of customer

--- src/infrastructure/api/__tests__/customer.e2e.spec.ts
+++ src/infrastructure/api/__tests__/customer.e2e.spec.ts
@@ -72,4 +72,95 @@ describe("E2E test for customer", () => {
     expect(findResponse.body.address.state).toBe("São Paulo");
     expect(findResponse.body.address.zipCode).toBe("09421-540");
   });
+
+  it("Should return all customers", async () => {
+    await request(app)
+      .post("/customer/create")
+      .send({
+        name: "John Doe",
+        address: {
+          street: "Av. Papa João XXIII, 695",
+          city: "Ribeirão Pires",
+          state: "São Paulo",
+          zipCode: "09421-540",
+        },
+      });
+
+    await request(app)
+      .post("/customer/create")
+      .send({
+        name: "Jane Doe",
+        address: {
+          street: "Rua das Flores, 123",
+          city: "São Paulo",
+          state: "São Paulo",
+          zipCode: "09421-540",
+        },
+      });
+
+    const findResponse = await request(app).get("/customer/list");
+
+    expect(findResponse.status).toBe(200);
+    expect(findResponse.body.customers.length).toBe(2);
+    expect(findResponse.body.customers[0].name).toBe("John Doe");
+    expect(findResponse.body.customers[0].address.street).toBe(
+      "Av. Papa João XXIII, 695"
+    );
+    expect(findResponse.body.customers[0].address.city).toBe("Ribeirão Pires");
+    expect(findResponse.body.customers[0].address.state).toBe("São Paulo");
+    expect(findResponse.body.customers[0].address.zipCode).toBe("09421-540");
+    expect(findResponse.body.customers[1].name).toBe("Jane Doe");
+    expect(findResponse.body.customers[1].address.street).toBe(
+      "Rua das Flores, 123"
+    );
+  });
+
+  it("Should update a customer", async () => {
+    const createResponse = await request(app)
+      .post("/customer/create")
+      .send({
+        name: "John Doe",
+        address: {
+          street: "Av. Papa João XXIII, 695",
+          city: "Ribeirão Pires",
+          state: "São Paulo",
+          zipCode: "09421-540",
+        },
+      });
+
+    const updateResponse = await request(app)
+      .put(`/customer/${createResponse.body.id}/update`)
+      .send({
+        name: "John Doe Updated",
+        address: {
+          street: "Rua das Flores, 123",
+          city: "São Paulo",
+          state: "São Paulo",
+          zipCode: "09421-540",
+        },
+      });
+
+    expect(updateResponse.status).toBe(200);
+    expect(updateResponse.body.name).toBe("John Doe Updated");
+  });
+
+  it("Should delete a customer", async () => {
+    const createResponse = await request(app)
+      .post("/customer/create")
+      .send({
+        name: "John Doe",
+        address: {
+          street: "Av. Papa João XXIII, 695",
+          city: "Ribeirão Pires",
+          state: "São Paulo",
+          zipCode: "09421-540",
+        },
+      });
+
+    const deleteResponse = await request(app).delete(
+      `/customer/${createResponse.body.id}/delete`
+    );
+
+    expect(deleteResponse.status).toBe(204);
+  });
 });
--- src/infrastructure/api/routes/customer.route.ts
+++ src/infrastructure/api/routes/customer.route.ts
@@ -2,6 +2,9 @@ import express, { Request, Response } from "express";
 import { CreateCustomerUseCase } from "../../../usecase/customer/create/create.customer.usecase";
 import { CustomerRepository } from "../../customer/repository/sequelize/customer.repository";
 import { FindCustomerUseCase } from "../../../usecase/customer/find/find.customer.usecase";
+import { ListCustomerUseCase } from "../../../usecase/customer/list/list.customer.usecase";
+import { UpdateCustomerUseCase } from "../../../usecase/customer/update/update.customer.usecase";
+import { DeleteCustomerUseCase } from "../../../usecase/customer/delete/delete.customer.usecase";
 
 export const customerRoute = express.Router();
 
@@ -35,6 +38,61 @@ customerRoute.get(
         id: request.params.id,
       });
       response.status(200).send(output);
+    } catch (err) {
+      response.status(500).send(err);
+    }
+  }
+);
+
+customerRoute.get("/list", async (request: Request, response: Response) => {
+  const useCase = new ListCustomerUseCase(new CustomerRepository());
+
+  try {
+    const output = await useCase.execute({});
+
+    response.status(200).send(output);
+  } catch (err) {
+    response.status(500).send(err);
+  }
+});
+
+customerRoute.put(
+  "/:id/update",
+  async (request: Request, response: Response) => {
+    const useCase = new UpdateCustomerUseCase(new CustomerRepository());
+
+    const customerDTO = {
+      name: request.body.name,
+      address: {
+        street: request.body.address.street,
+        city: request.body.address.city,
+        state: request.body.address.state,
+        zipCode: request.body.address.zipCode,
+      },
+    };
+
+    try {
+      const output = await useCase.execute({
+        id: request.params.id,
+        ...customerDTO,
+      });
+      response.status(200).send(output);
+    } catch (err) {
+      response.status(500).send(err);
+    }
+  }
+);
+
+customerRoute.delete(
+  "/:id/delete",
+  async (request: Request, response: Response) => {
+    const useCase = new DeleteCustomerUseCase(new CustomerRepository());
+
+    try {
+      await useCase.execute({
+        id: request.params.id,
+      });
+      response.status(204).send();
     } catch (err) {
       response.status(500).send(err);
     }
